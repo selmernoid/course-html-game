19a19fcd694e8ceed758d9fd1d3edff3a94b2c2d
DESKTOP-7A40RVD\Anton <selmernoid@mail.ru>
2021-03-19 23:03:24 +0200

    toggle class by click

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 let blocks = document.getElementsByClassName('block');
 for (let i = 0; i < blocks.length; i++) {
     const element = blocks[i];
-    element.addEventListener('click', () => {
-        alert('We did that!');
+
+    element.addEventListener('click', (event) => {
+        // const clickedElement = event.currentTarget
+        if (!element.classList.contains('active')) return;
+        // if (!element.classList.contains('flipped')) element.classList.add('flipped');
+        element.classList.toggle('flipped');
     });
 }
