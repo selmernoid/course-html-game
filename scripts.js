79933bb689db40a68634c75d871593164b599f4e
Artem Bohush <Artem_Bohush@epam.com>
2021-04-15 19:46:52 +0300

    feature/game-logic: implement game process

--- scripts.js
+++ scripts.js
@@ -1,33 +1,175 @@
-const rows = 8;
-const cells = 8;
+const gameInfo = {
+    score: 0,
+    shownPairs: [],
+    openedCards: [],
+    level: 1,
+    pairsRemain: 0,
+    health: 10,
+}
 
-//main game field element
-const gameElement = document.getElementById('game');
+const gameField = document.getElementById('game');
+const levelCounter = document.getElementById('level');
+const scoreCounter = document.getElementById('score');
+const healthCounter = document.getElementById('health');
 
-// dynamically create rows with cells
-let itemCounter = 0;
-for (let row = 0; row < rows; row++) {
-    const newRow = document.createElement('div');
-    newRow.className = 'grid-table-row';
-    gameElement.appendChild(newRow);
+generateLevel(gameInfo.level);
 
-    for (let cell = 0; cell < cells; cell++) {
-        const newCell = document.createElement('div');
-        newCell.className = 'block active';
-        newCell.innerText = itemCounter++;
-        newRow.appendChild(newCell);
+//
+
+function generateLevel(level) {
+    gameField.innerHTML = '';
+    levelCounter.innerHTML = level;
+    scoreCounter.innerHTML = gameInfo.score;
+    healthCounter.innerHTML = gameInfo.health;
+
+    for (let i = 0; i < 64; i++) {
+        const flipCard = document.createElement('div');
+        flipCard.className = 'flip-card';
+
+        const sideFront = document.createElement('div');
+        sideFront.className = 'flip-card-front';
+        const sideFrontImg = document.createElement('img');
+        sideFrontImg.setAttribute('src', './img/solid.jpg');
+        sideFrontImg.setAttribute('alt', 'avatar');
+        sideFront.appendChild(sideFrontImg);
+
+        const sideBack = document.createElement('div');
+        sideBack.className = 'flip-card-back';
+        const sideBackImg = document.createElement('img');
+        sideBackImg.setAttribute('alt', 'avatar');
+        sideBack.appendChild(sideBackImg);
+
+        flipCard.appendChild(sideFront);
+        flipCard.appendChild(sideBack);
+        flipCard.classList.add('invisible');
+        flipCard.setAttribute('id', i)
+
+        gameField.appendChild(flipCard);
+    }
+
+    const cards = Array.from(document.getElementsByClassName('flip-card'));
+
+    shuffle(cards);
+
+    const pairedCards = []
+    const imgNames = Array.from(new Array(32)).map((_, idx) => idx + 1);
+
+    for (let i = 0; i < 64; i += 2) {
+        const pair = [cards[i], cards[i + 1]]
+        const imgName = getFreeImgName(imgNames)
+        pair[0].lastElementChild.firstElementChild.setAttribute('src', `./img/svg/${imgName}.svg`);
+        pair[1].lastElementChild.firstElementChild.setAttribute('src', `./img/svg/${imgName}.svg`);
+        pairedCards.push(pair);
+    }
+
+    for (let i = 0; i < level; i++) {
+        const firstCard = pairedCards[i][0];
+        const secondCard = pairedCards[i][1];
+        gameInfo.shownPairs.push([firstCard, secondCard]);
+
+        firstCard.classList.remove('invisible');
+        firstCard.classList.add('visible');
+        firstCard.addEventListener('click', handleClick);
+
+        secondCard.classList.remove('invisible');
+        secondCard.classList.add('visible');
+        secondCard.addEventListener('click', handleClick);
+
+        setTimeout(() => {
+            secondCard.classList.add('flip');
+            firstCard.classList.add('flip');
+
+            setTimeout(() => {
+                secondCard.classList.remove('flip');
+                firstCard.classList.remove('flip');
+            }, 500 * level)
+        }, 300)
+    }
+
+    gameInfo.pairsRemain = gameInfo.shownPairs.length;
+}
+
+function handleClick(event) {
+    const card = event.currentTarget;
+
+    if (isSameCardClicked(card.getAttribute('id'))) {
+        return;
+    }
+
+    card.classList.add('flip');
+
+    gameInfo.openedCards.push(card);
+
+    if (gameInfo.openedCards.length === 2) {
+        setTimeout(() => {
+            if (isPairedCards()) {
+                hideGuessedCards();
+            } else {
+                closeBackCards()
+            }
+        }, 800)
     }
 }
 
-// assign click handlers
-let blocks = document.getElementsByClassName('block');
-for (let i = 0; i < blocks.length; i++) {
-    const element = blocks[i];
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+}
 
-    element.addEventListener('click', (event) => {
-        // const clickedElement = event.currentTarget
-        if (!element.classList.contains('active')) return;
-        // if (!element.classList.contains('flipped')) element.classList.add('flipped');
-        element.classList.toggle('flipped');
+
+function isSameCardClicked(clickedCardId) {
+    return !!(gameInfo.openedCards.find(c => c.getAttribute('id') === clickedCardId));
+}
+
+function isPairedCards() {
+    gameInfo.openedCards[0].getAttribute('id') === gameInfo.openedCards[1].getAttribute('id');
+    for (let pair of gameInfo.shownPairs) {
+        if (pair.includes(gameInfo.openedCards[0]) && pair.includes(gameInfo.openedCards[1])) {
+            return true
+        }
+    }
+    return false
+}
+
+function closeBackCards() {
+    gameInfo.openedCards.forEach(c => {
+        c.classList.remove('flip');
     });
+    gameInfo.openedCards = [];
+    gameInfo.health -= 1;
+    healthCounter.innerHTML = gameInfo.health;
+}
+
+function hideGuessedCards() {
+    gameInfo.openedCards.forEach(c => {
+        c.classList.remove('flip');
+        c.removeEventListener('click', handleClick);
+        setTimeout(() => {
+            c.classList.remove('visible');
+            c.classList.add('invisible');
+        }, 400)
+    });
+    gameInfo.openedCards = [];
+    gameInfo.score += 1;
+    gameInfo.pairsRemain -= 1;
+    scoreCounter.innerHTML = gameInfo.score;
+
+    setTimeout(() => {
+        if (gameInfo.pairsRemain === 0) {
+            gameInfo.level += 1;
+            gameInfo.shownPairs = [];
+            generateLevel(gameInfo.level)
+        }
+    }, 1500)
+}
+
+function getFreeImgName(names) {
+    let rand = Math.floor(0 + Math.random() * (names.length - 0));
+    const imgName = names[rand]
+    names.splice(rand, 1)
+    return imgName;
 }
